0e9efc7c2edb55920a2bced6b0f5b0360b949805
Katelyn Junker <kjunker@villanova.edu>
2018-10-19 00:12:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,5 +29,10 @@ function distanceTravelledInFeet(block1, block2){
 
 function calculatesFarePrice(start, destination){
   //how far//
+  const numberOfBlocks = Math.abs(startingBlock-destinationBlock)
+  const numberOfFeet= numberOfBlocks* numberInBlock
   
+  if (numberOfFeet <=400){
+    return 0
+  }
 }
